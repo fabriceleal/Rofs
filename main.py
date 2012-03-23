b1ccdfda49a7770e70e6a9b77b5ad5e370fec454
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-03-23 13:27:33 -0700

    added working fuse python script

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 
 
-from secret import TEST_SECRET
+from secret import *
 
 
 
-print TEST_SECRET
+#print TEST_SECRET
+
+from dropbox import client, rest, session
+
+sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
+
+request_token = sess.obtain_request_token()
+
+url = sess.build_authorize_url(request_token)
+
+print url
+raw_input()
+
+access_token = sess.obtain_access_token(request_token)
+
+client = client.DropboxClient(sess)
 
 
